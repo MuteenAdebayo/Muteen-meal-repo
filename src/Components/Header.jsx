4ce572a0a1607bd6d398f9591df97ad7bd9ca73d
MuteenAdebayo <146487421+MuteenAdebayo@users.noreply.github.com>
2023-12-01 21:03:07 +0100

    fixed an error

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+
 import { Link } from 'react-router-dom';
-import  '../styles/header.css'
+
 const Header = () => {
   return (
     <div>
@@ -20,19 +20,6 @@ const Header = () => {
           <Nav className="ms-auto">
              <Link to='/' className='text-success home'>HOME</Link>
              <Link to='/about' className='text-success  about'>ABOUT</Link>
-            {/* <Nav.Link href="#home">SIGNUP</Nav.Link>
-            <Nav.Link href="#link">LOGIN</Nav.Link> */}
-            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
